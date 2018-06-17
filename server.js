cbc7689bd9aefdfcf39ac3a31e10a56bd29ed2f7
Tony <looty89@gmail.com>
2018-06-17 08:38:41 +0800

    Changes to server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const aog = require('./dialogController')
+const aog = require('./dialogController');
 const app = express();
 
 app.use(app.json(), aog).listen(3000);
