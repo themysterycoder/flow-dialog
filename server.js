f13967b8f27fae280f74dcbffb7da173d569c8c2
Tony <looty89@gmail.com>
2018-06-17 08:27:30 +0800

    Changes to export

--- server.js
+++ server.js
@@ -2,5 +2,8 @@ const express = require('express');
 const aog = require('./dialogController.js')
 const app = express();
 
+app.post('/', function(req, res){
+  
+});
 
 app.use(app.json(), aog).listen(3000);
