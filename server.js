606e1385d32366f3f9cea618479aa900f8a74623
Tony <looty89@gmail.com>
2018-06-17 10:48:52 +0800

    adding yes no intents

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ const rp = require('request-promise');
 
 const aogApp = dialogflow();
 
-aogApp.intent('GetRecipe', conv => {
+aogApp.intent('Default Welcome Intent - yes', conv => {
   conv.ask(`<speak>Jiak Simi?</speak>`);
+});
+
+aogApp.intent('Default Welcome Intent - no', conv => {
+  conv.close(`<speak>Got it! See you soon! <audio src='https://actions.google.com/sounds/v1/human_voices/human_burp.ogg' /></speak>`);
 });
 
 const PORT = process.env.PORT || 3000;
