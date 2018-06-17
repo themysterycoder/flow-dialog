7fab7f0d85158e25c04cd49926e7203341c6d96c
Tony <looty89@gmail.com>
2018-06-17 11:46:50 +0800

    changes to processing

--- dialogController.js
+++ dialogController.js
@@ -4,8 +4,12 @@ const rp = require('request-promise');
 
 var aogApp = dialogflow();
 
-agoApp.intent('GetRecipe', conv => {
+aogApp.intent('Default Welcome Intent - yes', conv => {
   conv.ask(`<speak>Jiak Simi?</speak>`);
+});
+
+aogApp.intent('Default Welcome Intent - no', conv => {
+  conv.close(`<speak>Got it! See you soon! <audio src='https://actions.google.com/sounds/v1/human_voices/human_burp.ogg' /></speak>`);
 });
 
 
--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 const express = require('express');
-//const aog = require('./dialogController');
+const aog = require('./dialogController');
 const app = express();
 const bodyparser = require('body-parser');
 const {dialogflow} = require('actions-on-google');
 const rp = require('request-promise');
 
-
-const aogApp = dialogflow();
-
-aogApp.intent('Default Welcome Intent - yes', conv => {
-  conv.ask(`<speak>Jiak Simi?</speak>`);
-});
-
-aogApp.intent('Default Welcome Intent - no', conv => {
-  conv.close(`<speak>Got it! See you soon! <audio src='https://actions.google.com/sounds/v1/human_voices/human_burp.ogg' /></speak>`);
-});
+app.get('/hello', function(req,res){
+  res.send('Welcome to MasterChef');
+})
 
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyparser.json(), aogApp).listen(PORT, () => {
+app.use(bodyparser.json(), aog).listen(PORT, () => {
   console.log(`Our app is running on port ${ PORT }`);});
