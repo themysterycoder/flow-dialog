6ce9703478b0ae59d8cb876abe847644419f83a4
Tony <looty89@gmail.com>
2018-06-17 08:58:45 +0800

    Changes to server

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 //const aog = require('./dialogController');
 const app = express();
+const bodyparser = require('body-parser');
 const {dialogflow} = require('actions-on-google');
 const rp = require('request-promise');
 
 
-var aogApp = dialogflow();
+const aogApp = dialogflow();
 
 agoApp.intent('GetRecipe', conv => {
   conv.ask(`<speak>Jiak Simi?</speak>`);
 });
 
-app.use(app.json(), aogApp).listen(3000);
+app.use(bodyparser.json(), aogApp).listen(3000);
